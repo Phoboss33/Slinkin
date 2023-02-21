b2b275f658857fe1f300538243a2805fa126cba2
ThisPhoboss <102321702+Phoboss33@users.noreply.github.com>
2023-02-21 11:13:13 +0500

    Add files via upload

--- main.c
+++ main.c
@@ -10,85 +10,143 @@ int main() {
 
    char str[20]="";
    
-   gets (str);
-   printf ("Считана строка:\n%s\n",str);
+   gets(str);
+   printf ("Считана строка:\n%s\n", str);
    printf ("==========================\n");
    
    if (strcmp("unsigned char",str) == 0) {
 		printf("Байт: 1\n");
 		printf("Целочисленный тип\n");
 		printf("Штатный размер\n");
-		printf("Беззнаковый\n");
-		printf("Минимальная граница %d\n", UCHAR_MIN);
-		printf("Минимальная граница %d\n", UCHAR_MAX);
+		printf("Знаковый\n");
+		printf("Минимальная граница UCHAR_MIN: %d\n", UCHAR_MIN);
+		printf("Минимальная граница UCHAR_MAX: %d\n", UCHAR_MAX);
    }
    
-   if (strcmp("long double",str) == 0) {
+   else if (strcmp("long double",str) == 0) {
 		printf("Байт: 16\n");
 		printf("Вещественный тип\n");
 		printf("Длиный размер\n");
 		printf("Беззнаковый\n");
-		printf("Минимальная величина %Lg\n", LDBL_MIN);
-		printf("Максимальная величина %Lg\n", LDBL_MAX);
-		printf("Количество значащих цифр %d\n", LDBL_DIG);
-		printf("Минимальная погрешность %Lg\n", LDBL_EPSILON);
+		printf("Минимальная величина LDBL_MIN: %Lg\n", LDBL_MIN);
+		printf("Максимальная величина LDBL_MAX: %Lg\n", LDBL_MAX);
+		printf("Количество значащих цифр LDBL_DIG: %d\n", LDBL_DIG);
+		printf("Минимальная погрешность LDBL_EPSILON: %Lg\n", LDBL_EPSILON);
 	}
 	
-   if (strcmp("int",str) == 0) || strcmp("short int",str) == 0 || strcmp("signed short",str) == 0 || strcmp("signed short int",str) == 0) {
+	else if (strcmp("int",str) == 0 || strcmp("signed int",str) == 0 || strcmp("signed",str) == 0) {
 		printf("Байт: 4\n");
 		printf("Целочисленный тип\n");
-		printf("Штатный размер\n");  // Доделать
-		printf("Беззнаковый\n");
+		printf("Штатный размер\n");  	
 		printf("Минимальная граница %d\n", INT_MIN);
 		printf("Минимальная граница %d\n", INT_MAX);
 	}  
 	
-   if (strcmp("float",str) == 0) {
+	else if (strcmp("unsigned", str) == 0 || strcmp("unsigned int", str) == 0) {
+		printf("Байт: 2\n");
+		printf("Целочисленный тип\n");
+		printf("Короткий размер\n");
+		printf("Беззнаковый\n");
+		printf("Нижняя граница %d\n", 0);
+		printf("Верхняя граница UINT_MAX: %u\n", UINT_MAX);
+	}
+	
+
+
+	
+   else if (strcmp("float",str) == 0) {
 		printf("Байт: 4\n");
 		printf("Вещественный тип\n");
 		printf("Штатный размер\n");
-		printf("Беззнаковый\n");
-		printf("Минимальная величина %g\n", FLT_MIN);
-		printf("Максимальная величина %g\n", FLT_MAX);
-		printf("Количество значащих цифр %d\n", FLT_DIG);
-		printf("Минимальная погрешность %g\n", FLT_EPSILON);
+		printf("Знаковый\n");
+		printf("Минимальная величина FLT_MIN: %g\n", FLT_MIN);
+		printf("Максимальная величина FLT_MAX: %g\n", FLT_MAX);
+		printf("Количество значащих цифр FLT_DIG: %d\n", FLT_DIG);
+		printf("Минимальная погрешность FLT_EPSILON: %g\n", FLT_EPSILON);
 	}
 	
-   if (strcmp("double",str) == 0) {
-		printf("Байт: 4\n");
+   else if (strcmp("double",str) == 0) {
+		printf("Байт: 8\n");
 		printf("Вещественный тип\n");
 		printf("Длиный размер\n");
-		printf("Беззнаковый\n");
-		printf("Минимальная величина %g\n", DBL_MIN);
-		printf("Максимальная величина %g\n", DBL_MAX);
-		printf("Количество значащих цифр %d\n", DBL_DIG);
-		printf("Минимальная погрешность %g\n", DBL_EPSILON);
+		printf("Знаковый\n");
+		printf("Минимальная величина DBL_MIN: %g\n", DBL_MIN);
+		printf("Максимальная величина DBL_MAX: %g\n", DBL_MAX);
+		printf("Количество значащих цифр DBL_DIG: %d\n", DBL_DIG);
+		printf("Минимальная погрешность DBL_EPSILON: %g\n", DBL_EPSILON);
 	}  
 	 
-    if (strcmp(str, "char") == 0) {
+    else if (strcmp(str, "char") == 0) {
 		printf("Байт: %d \n", CHAR_BIT / 8);
 		printf("Целочисленный тип\n");
 		printf("Штатный размер\n");
-		printf("Беззнаковый\n");
-		printf("Минимальная величина %d\n", CHAR_MIN);
-		printf("Максимальная величина %d\n", CHAR_MAX);
+		printf("Беззнаковый\n"); 	// Знаковый или беззнаковый
+		printf("Нижняя граница CHAR_MIN: %d\n", CHAR_MIN);
+		printf("Верзняя граница CHAR_MAX: %d\n", CHAR_MAX);
+	}  
+	 
+    else if (strcmp(str, "signed char") == 0) {
+		printf("Байт: %d \n", CHAR_BIT / 8);
+		printf("Целочисленный тип\n");
+		printf("Штатный размер\n");
+		printf("Знаковый\n"); 
+		printf("Нижняя граница SCHAR_MIN: %d\n", SCHAR_MIN);
+		printf("Верзняя граница SCHAR_MAX: %d\n", SCHAR_MAX);
 	}  
 	
-	if ((strcmp("short", str) == 0) || (strcmp("short int", str) == 0) || (strcmp("signed short", str) == 0) || (strcmp("signet short int", str) == 0))  {
+	else if ((strcmp("short", str) == 0) || (strcmp("short int", str) == 0) || (strcmp("signed short", str) == 0) || (strcmp("signed short int", str) == 0))  {
 		printf("Байт: 2\n");
-		printf("Целочисленный тип \n");
-		printf("Короткий размер \n");
-		printf("Нижняя граница: SHRT_MIN = %d \n", SHRT_MIN);
-		printf("Верхняя граница: SHRT_MAX = %d \n", SHRT_MAX);
+		printf("Целочисленный тип\n");
+		printf("Короткий размер\n");
+		printf("Знаковый\n");
+		printf("Нижняя граница SHRT_MIN: %hi\n", SHRT_MIN);
+		printf("Верхняя граница SHRT_MAX: %hi\n", SHRT_MAX);
+	}
+	
+	else if (strcmp("unsigned short", str) == 0 || strcmp("unsigned short int", str) == 0) {
+		printf("Байт: 2\n");
+		printf("Целочисленный тип\n");
+		printf("Короткий размер\n");
+		printf("Беззнаковый\n");
+		printf("Нижняя граница: %d\n", 0);
+		printf("Верхняя граница USHRT_MAX: %d\n", USHRT_MAX);
 	}
 
-	if (strcmp("unsigned short", str) == 0 || strcmp("unsigned short int", str) == 0) {
-		printf("Байт: 2");
-		printf("Целочисленный тип \n");
-		printf("Короткий размер \n");
-		printf("Беззнаковый \n");
-		printf("Нижняя граница: USHRT_MIN = %d \n", 0);
-		printf("Верхняя граница: USHRT_MAX = %d \n", USHRT_MAX);
+	else if ((strcmp("long", str) == 0) || (strcmp("long int", str) == 0) ||
+		(strcmp("signed long", str) == 0) || (strcmp("signed long int", str) == 0)) {
+		printf("Байт: 8");
+		printf("Целочисленный тип\n");
+		printf("Длинный размер\n");
+		printf("Нижняя граница LONG_MIN: %li\n", LONG_MIN);
+		printf("Верхняя граница LONG_MAX: %li\n", LONG_MAX);
+	}
+
+	else if ((strcmp("unsigned long", str) == 0) || (strcmp("unsigned long int", str) == 0)) {
+		printf("Байт: 8");
+		printf("Целочисленный тип\n");
+		printf("Длинный размер\n");
+		printf("Беззнаковый\n");
+		printf("Нижняя граница: %d\n", 0);
+		printf("Верхняя граница ULONG_MAX: %lu\n", ULONG_MAX);
+	}
+
+	else if ((strcmp("long long", str) == 0) || (strcmp("long long int", str) == 0) ||
+		(strcmp("signed long long", str) == 0) || (strcmp("signed long long int", str) == 0)) {
+		printf("Байт: 8\n");
+		printf("Целочисленный тип\n");
+		printf("Знаковый\n");
+		printf("Длинный длинный размер\n");
+		printf("Нижняя граница LLONG_MIN: %lli\n", LLONG_MIN);
+		printf("Верхняя граница LLONG_MAX: %lli\n", LLONG_MAX);
+	}
+
+	else if ((strcmp("unsigned long long", str) == 0) || (strcmp("unsigned long long int", str) == 0)) {
+		printf("Байт: 8");
+		printf("Целочисленный тип\n");
+		printf("Длинный длинный размер\n");
+		printf("Беззнаковый\n");
+		printf("Нижняя граница: %d\n", 0);
+		printf("Верхняя граница ULLONG_MAX: %llu\n", ULLONG_MAX);
 	}
 	else
 		printf("Error\n");
