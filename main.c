17210f32cf5b262a2af0c44826332d6064efe626
ThisPhoboss <102321702+Phoboss33@users.noreply.github.com>
2023-02-27 10:21:32 +0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+//#include <locale.h>
+#include <limits.h>
+#include <float.h>
+
+#define UCHAR_MIN 0
+
+int main() {
+	printf("Successfull");
+	return 0;
+}
+
