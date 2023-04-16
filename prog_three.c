af98b4e643bbf12cd6ad4521d87b63f8d6920c19
Phoboss <102321702+Phoboss33@users.noreply.github.com>
2023-04-16 22:06:52 +0500

    Add files via upload

--- prog_three.c
+++ prog_three.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
+
+int isGreater(int x, int y) {
+    int diff = y - x;
+    int sign = (diff >> 31) & 1;
+    int check = !(diff ^ 0);
+    return (sign ^ 1) & 1 & !check;
+}
 
 int main() {
-    
+    int mas[7] = {1, 2, 3, 2, 1, 2, 3};
+    int res[7] = {};
+    int sizemas = (sizeof(mas) / sizeof(int)) - 1;
+
+    for (size_t i = 0;i < sizemas;i++) {
+        int kek = isGreater(mas[i],mas[i+1]);
+        res[i] = kek; 
+        printf("%d\n", kek);
+        //printf("res = %d\n", res);
+    }
+
+    int result[] = {};
+    int lol;
+
+    for (int i = 0;i < sizemas;i++) {
+        lol += res[i] ^ res[sizemas-1-i];
+        printf("res %d re %d\n",res[i], lol);
+        
+    }
+
+    if (lol == 0 )
+        printf("true\n");
+    else 
+        printf("false\n");
+    return 0;
 }
