16325255237938efa2e4a8c761ebc0b5dbda3814
Phoboss <102321702+Phoboss33@users.noreply.github.com>
2023-04-16 23:25:53 +0500

    Add files via upload

--- prog_two.c
+++ prog_two.c
@@ -1,34 +1,18 @@
 #include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
-
-void func(short N, short len) {
-    short mas[len];
-
-    printf("print number: \n");
-
-    for (int i = 0;i < len;i++) {
-        scanf("%hd",&N);
-        mas[i] = N;
-    }
-
-    for (int i = 0;i < len;i++) {
-        printf("mas[%d]:= %hd\n",i,mas[i]);
-    }
-    if (mas[0] < mas[1]) {      // mas[len-1]
-        printf("является\n"); 
-    }
-    else {
-        printf("err\n"); 
-    }
-}
 
 int main() {
-    short N = 0, len = 0;
+    int arr[] = {4, 8, 10, 8, 4};
+    int a = arr[0];
+    int b = arr[1];
+    int c = arr[2];
+    int d = arr[3];
+    int e = arr[4];
 
-    printf("input number N > 1, [one: len, two: your number]\n");
-    scanf("%hd",&len);
-    printf("len:= %hd\n",len);
+    if (((a <= b) && (b <= c)) && ((d >= e) && (c >= d) && (b >= e))) {
+        printf("Первые три элемента массива расположены по возрастанию, следующие три - по убыванию\n");
+    } else {
+        printf("Первые три элемента массива НЕ расположены по возрастанию, либо следующие три НЕ расположены по убыванию\n");
+    }
 
-    func(N,len);
+    return 0;
 }
