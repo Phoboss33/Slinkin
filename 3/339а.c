bef03934d202bd204a597d1bc2f2b3bf3b3b5df7
Phoboss <102321702+Phoboss33@users.noreply.github.com>
2023-05-22 01:34:29 +0500

    339ac finish

--- 3/339а.c
+++ 3/339а.c
@@ -1,37 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int N = 0;
-	
-	// Ввод последовательности
-	printf("Введите длину последовательности:\n");
-	scanf(" %d",&N);
-	
+  
+    // Ввод последовательности
+    printf("Введите длину последовательности:\n");
+    scanf("%d", &N);
+  
     int a[N];
 
     printf("Введите последовательность:\n");
-    for (int i = 0; i < N;i++) {
-		scanf("%d", &a[i]);
-	}
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &a[i]);
+    }
 
-	// Основной код
+    int new = 0;
+
+    // Основной код
     printf("(a):");
-    for (size_t i = 0; i < N;i++) { // a
+    for (int i = 0; i < N; i++) { // a
         int num = 0;
-        for (size_t j = 0;j < N;j++)
-            if (a[i] == a[j])
+        for (int j = 0; j < N; j++)
+            if (a[i] == a[j]) {
                 num++;
+                new = num;
+            }
         if (num == 1)
-            printf("%d ",a[i]);
+            printf("%d ", a[i]);
     }
 
     printf("\n==============\n");
 
-    int kek = 1;
-    for (int i = 1;i < N;i++) {
-        if ((a[0] ^ a[i]) != 0)
-            kek++;
+    for (int i = 0; i < N-1; i++) {
+        for (int j = 0; j < N-i-1; j++) {
+            if (a[j] > a[j+1]) {
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
     }
-    
-    printf("(в):%d ",kek);
+
+    int num = 0;
+    for (int i = 0;i < N;i++)
+        if (a[i] != a[i + 1])
+            num++;
+
+    printf("\nРазличных чисел: %d",num);
+
+    return 0;
 }
