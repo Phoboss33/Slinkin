e765fd4791f47569f2df29f928580cb8171fe9d4
Phoboss <102321702+Phoboss33@users.noreply.github.com>
2023-12-26 12:42:30 +0500

    sort?

--- block_2/vor.c
+++ block_2/vor.c
@@ -8,6 +8,18 @@
 #include <sys/file.h>
 #include <time.h>
 
+void BubbleSort(int* arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                unsigned char temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     const char* filename = "secure.dat";
     int interval = atoi(argv[1]);
@@ -104,7 +116,10 @@ int main(int argc, char *argv[]) {
         }
 
     }
-
+	BubbleSort(positions, fileSize);	
+	for (int i = 0;i < fileSize;i++) {
+		printf("%d", positions[i]);
+	}
     
         //usleep(interval * 1000);
 
