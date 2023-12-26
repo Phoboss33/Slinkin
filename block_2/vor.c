81568a5205a0b3150f19e943a13f24b809afc0c0
Phoboss <102321702+Phoboss33@users.noreply.github.com>
2023-12-26 10:45:51 +0500

    make rand

--- block_2/vor.c
+++ block_2/vor.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <string.h>
 #include <sys/file.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
     const char* filename = "secure.dat";
@@ -34,18 +35,70 @@ int main(int argc, char *argv[]) {
             usleep(1000000);
         }
     }
+    int fileSize = lseek(file, 0, SEEK_END) - 1;
+    printf("Размер файла: %d байт\n", fileSize + 1);
 
-    struct stat st;
-    fstat(file, &st);
-    int size = st.st_size;
+    srand(time(NULL));
+    int positions[fileSize];
+    int needRandPos = 0;
+    int randPos;
+    int count = 0;
+    close(file);
 
 
-    printf("Размер файла: %d байт\n", size);
-    unsigned char byteRead;
+    while(count < fileSize) {
+        randPos = rand() % fileSize;
+
+        int flag = 0;
+        for (int i = 0;i < count;i++) {
+            if (positions[i] == randPos) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (!flag) {
+            positions[count++] = randPos;
+        }
+
+    }
+    for (int i = 0;i < fileSize;i++) {
+        printf("%d ", positions[i]);
+    }
+
+
+    unsigned char buffer[1];
+    while (1) {
+        usleep(10000);
+        
+        file = open(filename, O_RDWR);
+        if (file != -1) {
+            
+
+            lseek(file, randPos, SEEK_SET);
+            int bytesRead = read(file, buffer, 1);
+            printf("\nПрочитан: [%ld] %d\n", needRandPos, buffer[0]);
+            //positions[0] = needRandPos;
+
+            lseek(file, needRandPos, SEEK_SET);
+            char zero = 0;
+
+            int bytesWrite = write(file, &zero, 1);
+
+            close(file);
+        }
+        else {
+            printf("Тревога, уходим!");
+            break;
+        }
+
+    }
+
+    
         //usleep(interval * 1000);
 
-    read(file, &byteRead, 1);
-    write(file, 0, 1);
+    //read(file, &byteRead, 1);
+    //write(file, 0, 1);
     
 
     return 0;
